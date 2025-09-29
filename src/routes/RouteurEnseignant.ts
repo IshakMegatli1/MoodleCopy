f4af4e3d1812708488695d17c04ff9c6eaeffa2d
DanielAtik1 <150082600+DanielAtik1@users.noreply.github.com>
2025-09-28 22:18:53 -0400

    Authentification non-fonctionnelle, À refaire

--- src/routes/RouteurEnseignant.ts
+++
@@ -1,7 +0,0 @@
-import { Router, Request, Response, NextFunction } from 'express';
-import { Enseignant } from '../core/enseignant';
-import { InvalidParameterError } from '../core/errors/invalidParameterError';
-
-// export class RouteurEnseignant {
-//   private _router: Router
-  
